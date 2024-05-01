f0e956f69100db8f169d3455564a4b19be787424
muhammadfirmanazhary <muhammadfirmanazhary.com>
2024-05-01 20:48:25 +0700

    first

---
+++ routes/web.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::view('/', 'frontend.index')->name('home');
+Route::view('/details', 'frontend.details')->name('detail');
+Route::view('/checkout', 'frontend.checkout')->name('checkout');
+Route::view('/checkout-success', 'frontend.checkout-success')->name('checkout-success');
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
